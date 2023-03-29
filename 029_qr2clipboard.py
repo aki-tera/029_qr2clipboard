ab23729454933e51c0c9446d0ea0c0c0266c0a3b
aki-tera <aki.tera@gmail.com>
2023-03-29 21:15:14 +0900

    #1 雛形作成

--- 029_qr2clipboard.py
+++ 029_qr2clipboard.py
@@ -1,3 +1,96 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+from tkinter import font
+
 import cv2
-from pyzbar.pyzbar import decode, ZBarSymbol
+import numpy as np
+
+
+class Model:
+    """Webcam関連
+    1. カメライメージの取得
+    2. QRコードをハイライト
+    3. QRコードの値を取得
+    4. カメラ解放
+    """
+
+    def __init__(self):
+        """load webcams's config which is setting.json.
+        1. load setting.json.
+        2. create instance variables.
+        """
+
+
+class View:
+    """tkinter関連
+    """
+
+    def __init__(self, master, model):
+        """create widget objects
+        1. setiing of font
+        2. create menu objects
+        3. create frames objects and others
+        4. use grid()
+        Args:
+            master (class): main window
+            model (class): Webcam related
+        """
+
+
+class Controller():
+    """制御関連
+    """
+    def __init__(self, master, model, view):
+        """create instances
+        Args:
+            master (class): main window
+            model (class): Webcam related
+            view (class): tkinter related
+        """
+
+
+class Application(tk.Frame):
+    """メイン
+    1. tkinterを生成
+    2. ウインドウを設定
+    3. クラスオブジェクトを生成
+    4. ボタンコマンドを設定
+    Args:
+        tk (class): root of tkinter
+    """
+    def __init__(self, master):
+        """start instance
+        Args:
+            master  (class): main
+        """
+        # tkinterの定型文
+        super().__init__(master)
+        self.grid()
+        
+        # インスタンス化
+        self.model = Model()
+        
+        master.geometry()
+        master.title("QRコード2クリップボード")
+
+        # ウインドウサイズの変更不可
+        master.resizable(width=False, height=False)
+
+        # インスタンス化
+        self.view = View(master, self.model)
+        self.controller = Controller(master, self.model, self.view)
+
+        # ボタンのコマンド設定
+        # self.view.button31["command"] = self.controller.press_start_button
+        # self.view.button32["command"] = self.controller.press_close_button
+
+
+def main():
+    win = tk.Tk()
+    app = Application(master=win)
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
 
